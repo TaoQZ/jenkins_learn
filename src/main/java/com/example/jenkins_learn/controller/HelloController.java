d087edca7be39ea9cea8cca902f116275340ace7
taoqz <2538474354@qq.com>
2021-05-05 17:30:12 +0800

    init

---
+++ src/main/java/com/example/jenkins_learn/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.jenkins_learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ *
+ * @author taoqz
+ * @date 2021/5/5 17:26
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String demo(){
+        return "hello";
+    }
+}
