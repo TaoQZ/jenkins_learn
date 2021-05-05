30dedf7fb4dbf623401f0287b8c80d23cce558d5
taoqz <2538474354@qq.com>
2021-05-05 22:40:21 +0800

    succ

--- src/main/java/com/example/jenkins_learn/controller/HelloController.java
+++ src/main/java/com/example/jenkins_learn/controller/HelloController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String demo(){
-        return "success!!!!!!";
+        return "上班!!!";
     }
 }
