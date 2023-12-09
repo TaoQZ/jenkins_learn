6d5fcba98eec0dfe316ff614fc63a18520c7d27f
TaoQZ <37150992+TaoQZ@users.noreply.github.com>
2023-12-09 22:44:46 +0800

    Update HelloController.java

--- src/main/java/com/example/jenkins_learn/controller/HelloController.java
+++ src/main/java/com/example/jenkins_learn/controller/HelloController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String demo(){
-        return "真的是最后!1!";
+        return "hello jenkins!!!";
     }
 }
